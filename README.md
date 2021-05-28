1781e1cc29dca049687cc0f5596a49d70db37219
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-05-28 16:24:43 -0400

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaBlogProject
+create a blogpost app with full CRUD
+Java Spring Boot app 
---
+++ src/main/java/com/tts/TechTalentBlog/controller/BlogPostController.java
@@ -0,0 +1,75 @@
+package com.tts.TechTalentBlog.controller;
+
+import com.tts.TechTalentBlog.model.BlogPost;
+import com.tts.TechTalentBlog.service.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BlogPostController {
+
+    @Autowired
+    BlogPostService blogPostService;
+
+//    public BlogPostController(BlogPostService blogPostService) {
+//        this.blogPostService = blogPostService;
+//    }
+
+    @GetMapping("/")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts",blogPostService.getAllBlogPosts());
+        // this return value is a reference to a template
+        // it will not literally return a string value
+        return "blogpost/index";
+    }
+
+    @GetMapping("/blogposts/new")
+    public String newBlog (BlogPost blogPost){
+
+        return "blogpost/new";
+    }
+
+    @PostMapping("/blogposts")
+    public String addNewBlogPost(BlogPost blogPost, Model model) {
+        blogPostService.addNewBlogPost(blogPost);
+        model.addAttribute("title", blogPost.getTitle());
+        model.addAttribute("author", blogPost.getAuthor());
+        model.addAttribute("blogEntry", blogPost.getBlogEntry());
+        return "blogpost/result";
+    }
+
+//    @PostMapping
+//    public String addNewBlogPost(BlogPost blogPost) {
+//        blogPostService.addNewBlogPost(blogPost);
+//        return "blogpost/result";
+//    }
+
+    @DeleteMapping("/blogposts/{id}")
+    public String deletePostWithId(@PathVariable Long id){
+        blogPostService.deletePostById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/blogposts/{id}")
+    public String editPostWithId(@PathVariable Long id, Model model) {
+        BlogPost foundPost = blogPostService.findBlogPostById(id);
+        model.addAttribute("blogPost", foundPost);
+        return "blogpost/edit";
+    }
+
+    @PostMapping("/blogposts/update/{id}")
+    public String updateExistingPost(@PathVariable Long id,
+                                     BlogPost blogPost,
+                                     Model model) {
+        BlogPost editedPost = blogPostService.editBlogPostById(id, blogPost);
+        model.addAttribute("blogPost", editedPost);
+
+        return "blogpost/result";
+    }
+
+}
